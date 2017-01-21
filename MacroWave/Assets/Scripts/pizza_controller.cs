498f9e1f11bb4d80b0e2b23aa54212ffb1303268
Jonathan Alcantara <jfalcant@ucsc.edu>
2017-01-20 23:15:56 -0800

    HealthUI Update
    HealthUI included! Doesn't have the proper graphics yet.
    Left mouse click to drain health, right mouse click to gain health.

---
+++ MacroWave/Assets/Scripts/pizza_controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pizza_controller : MonoBehaviour {
+	private GameObject player;
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("player");//find and get player
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float step = speed * Time.deltaTime;
+		var player_pos = player.transform.position;
+		player_pos += new Vector3 (0, 0, 0);//location to move to in relation to player
+		transform.position = Vector3.MoveTowards(transform.position, player_pos, step);
+	}
+}
