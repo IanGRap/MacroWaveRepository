e49e3911d8d8ce00c77c5ba34a4ea4d98d149a7f
Ian Rapoport <IanGRap@gmail.com>
2017-01-20 22:56:58 -0800

    Cooking QTE

---
+++ MacroWave/Assets/CookingEvent/Scripts/CookingEventEndCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingEventEndCollider : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private CookingEventMaster master;
+
+	// Use this for initialization
+	void Start () {
+		master = GetComponentInParent <CookingEventMaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.tag == "cursor") {
+			master.Deactivate ();
+			master.MissedCursor ();
+		}
+	}
+}
---
+++ MacroWave/Assets/CookingEvent/Scripts/CookingEventMaster.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingEventMaster : MonoBehaviour {
+
+	public CookingEventBox[] EventBoxes;
+	public CookingEventCursor cursor;
+
+	private SpriteRenderer background;
+	private bool active = false;
+	private string cursorStatus = null;
+
+	void Start(){
+		background = GetComponentInChildren <SpriteRenderer> ();
+		background.enabled = false;
+		cursor = GetComponentInChildren <CookingEventCursor> ();
+		for (int i = 0; i < EventBoxes.Length; i++) {
+			EventBoxes [i].SetSprite (false);
+		}
+	}
+
+	void Update(){
+		if (active) {
+			if (Input.GetButtonDown ("action")) {
+				CursorHit ();
+			}
+		}
+	}
+
+	public void Activate(float speed){
+		active = true;
+		for (int i = 0; i < EventBoxes.Length; i++) {
+			EventBoxes [i].SetSprite (true);
+		}
+		background.enabled = true;
+		cursor.Activate (speed);
+	}
+
+	public void Deactivate(){
+		cursorStatus = null;
+		active = false;
+		for (int i = 0; i < EventBoxes.Length; i++) {
+			EventBoxes [i].SetSprite (false);
+		}
+		background.enabled = false;
+		cursor.Deactive ();
+	}
+
+	public bool CheckActive(){
+		return active;
+	}
+
+	public void UpdateCursor(string status){
+		if (status == "yellow") {
+			cursorStatus = "yellow";
+		} else if (status == "green") {
+			cursorStatus = "green";
+		} else if (status == "red") {
+			cursorStatus = "red";
+		}
+	}
+
+	// Kyle This Part
+	void CursorHit(){
+		if (cursorStatus == "yellow") {
+			//release enemy
+			Debug.Log ("Yellow");
+		} else if (cursorStatus == "green") {
+			//heal player
+			Debug.Log ("Green");
+		} else if (cursorStatus == "red") {
+			//fire enemy as projectile
+			Debug.Log ("Red");
+		} else
+		Deactivate ();
+	}
+
+	public void MissedCursor(){
+		//Whatever happens if the cursor runs of the end without being hit
+	}
+}
