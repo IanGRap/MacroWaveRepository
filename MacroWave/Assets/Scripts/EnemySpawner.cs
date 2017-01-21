0cd90591cf250e11daa32aea50b3d09c58a5ef1a
Whotsit <jacob.phong.le@gmail.com>
2017-01-21 12:28:03 -0800

    enemy spawner

---
+++ MacroWave/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿//This is the fault of Andrew
+using UnityEngine;
+using System.Collections;
+namespace Application
+{
+	public class EnemySpawner : MonoBehaviour
+	{
+		private GameObject Spawner;
+
+		//Attaches to the Spawner GameObject
+		void Start(){
+			Spawner = GameObject.Find("Spawner");
+		}
+
+		//Spawns enemy at the Spawn Gate's position
+		public void SpawnEnemy(GameObject Enemy){
+			Instantiate (Enemy, Spawner.transform.position);
+		}
+
+	}
+}
