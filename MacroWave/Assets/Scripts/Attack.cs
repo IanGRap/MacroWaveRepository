44e1e723e0661afb1aeb199dfe4314f23f3499c2
Kaos <kaoppenh@ucsc.edu>
2017-01-20 23:39:52 -0800

    attempt

---
+++ MacroWave/Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public object Michael;
+    bool full = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            if (!full)
+            {
+                transform.Translate(Input.mousePosition);
+            }
+        }
+	}
+}
