30aa74fa730ccc18be7f5fb4ddc60b37db059a3b
Jonathan Alcantara <jfalcant@ucsc.edu>
2017-01-21 17:09:16 -0800

    Clamp Update
    Player movement is restricted to the white plane. You can adjust the
    boundaries as necessary by changing it in the Player Inspector, movement
    script, Boundary.

--- MacroWave/Assets/Scripts/player_movement.cs
+++ MacroWave/Assets/Scripts/player_movement.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
 public class player_movement : MonoBehaviour {
 	
 	public float speed = 0.25f;
+	public Boundary boundary;
+	private Rigidbody2D body;
 
 	// Use this for initialization
 	void Start () {
-		
+		body = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +26,11 @@ public class player_movement : MonoBehaviour {
 		var movement = new Vector3 (moveHorizontal, moveVertical, 0);
 		transform.position += movement * speed; 
 
-
-		
+		body.position = new Vector3
+		(
+			Mathf.Clamp (body.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp (body.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+		);
 	}
 }
