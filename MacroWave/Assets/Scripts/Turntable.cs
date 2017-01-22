b5a9e4f5eec7e240f068145f414a875d6358ef8a
Kaos <kaoppenh@ucsc.edu>
2017-01-22 12:34:38 -0800

    music

---
+++ MacroWave/Assets/Scripts/Turntable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turntable : MonoBehaviour {
+
+    public AudioClip Jomesic;
+    public AudioClip Hughesic;
+    AudioSource Sona;
+
+	// Use this for initialization
+	void Start () {
+        Sona = GetComponent<AudioSource>();
+        float track = Mathf.Floor(Random.Range(0.0f, 1.999f));
+        if (track == 0)
+        {
+            Sona.clip = Jomesic;
+        }
+        else
+        {
+            Sona.clip = Hughesic;
+        }
+        Sona.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
