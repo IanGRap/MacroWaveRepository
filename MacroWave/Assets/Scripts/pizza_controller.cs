185c1a1190fe5c65e0a62c86d86a62bcdbf3ae77
Whotsit <jacob.phong.le@gmail.com>
2017-01-21 16:41:54 -0800

    More Enemy scripts

--- MacroWave/Assets/Scripts/pizza_controller.cs
+++ MacroWave/Assets/Scripts/pizza_controller.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 
 public class pizza_controller : MonoBehaviour {
 	private GameObject player;
+	private bool charging = false;
+	private bool chargeReady = true;
+	private Vector3 target_pos;
+	private float chargeRate = 1.0f;
+	private float nextCharge = 0.0f;
+
+
 	public float speed = 5.0f;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("player");//find and get player
+
 	}
+
+
+		
+	
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		float step = speed * Time.deltaTime;
 		var player_pos = player.transform.position;
-		player_pos += new Vector3 (0, 0, 0);//location to move to in relation to player
-		transform.position = Vector3.MoveTowards(transform.position, player_pos, step);
+		float distanceToPlayer = Mathf.Sqrt(Mathf.Pow((transform.position.x -player_pos.x),2) + Mathf.Pow((transform.position.y -player_pos.y),2));
+		//print (distanceToPlayer);
+		if (distanceToPlayer <= 5.0f && !charging && chargeReady) {
+			charging = true;
+			chargeReady = false;
+			target_pos = player.transform.position;
+
+			print ("charging!");
+		}
+		if (charging == false && chargeReady) {
+			target_pos = player.transform.position;//update target pos to player pos
+			print("updating target_pos");
+		}
+
+		float distanceToTarget = Mathf.Sqrt(Mathf.Pow((transform.position.x -target_pos.x),2) + Mathf.Pow((transform.position.y -target_pos.y),2));
+		//print (distanceToTarget);
+		if (distanceToTarget <= 0.1 && charging == true) {
+			charging = false;
+			nextCharge = Time.time + chargeRate;
+			print ("finished charge");
+
+		}
+		if (!charging && !chargeReady) {
+			Vector3 vectorToTarget = player.transform.position - transform.position;
+			float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+			Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+			if (Time.time > nextCharge) {
+				chargeReady = true;
+				print("charge ready!");
+			}
+		}
+			
+		transform.position = Vector3.MoveTowards(transform.position, target_pos, step);//move towards function
 	}
 }
