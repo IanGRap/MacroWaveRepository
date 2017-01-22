cadb4007c9418e896d97f884508febddbe3e96b5
Christopher Paz <cpaz1@ucsc.edu>
2017-01-21 20:13:22 -0800

    Stuff

---
+++ MacroWave/Assets/Scripts/StartButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    private Button start;
+	// Use this for initialization
+	void Start ()
+    {
+        start = GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
