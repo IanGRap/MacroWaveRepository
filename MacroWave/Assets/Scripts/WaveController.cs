e32baffcdf4844f33982b0701ae78f377f4fd9d9
Whotsit <jacob.phong.le@gmail.com>
2017-01-21 16:06:11 -0800

    Non working wave controller

---
+++ MacroWave/Assets/Scripts/WaveController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using System;
+
+namespace Application
+{
+    public class WaveController : MonoBehaviour
+    {
+        private int waveCount;
+        private bool activeWave;
+        private GameObject[] Spawners; //List that holds all enemy spawners
+
+        void Start()
+        {
+            Spawners = GameObject.FindObjectsOfType("EnemySpawner"); //NEED SPAWNER PREFAB
+            waveCount = 0;
+            activeWave = false;
+        }
+
+        void update()
+        {
+            foreach (GameObject spawner in Spawners)
+            {
+                if (spawner.active)
+                {
+                    waveCount++;
+                }
+            }
+        }
+    }
+}
