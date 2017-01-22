bdb8f45d522ca7c8fd1d4c676cb3bbfaa6d1eed0
Thomas <tccbryant@gmail.com>
2017-01-22 14:24:17 -0800

    main menu
    added quit button to main menu

---
+++ MacroWave/Assets/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class QuitButton : MonoBehaviour {
+    public void OnMouseDown()
+    {
+        EditorApplication.isPlaying = false;
+        //Application.Quit();
+    }
+}
