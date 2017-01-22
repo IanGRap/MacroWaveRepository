66aec23c93caef19501c6bf91bce3587e9a72316
Whotsit <jacob.phong.le@gmail.com>
2017-01-21 16:05:33 -0800

    Enemy Spawner

--- MacroWave/Assets/Scripts/EnemySpawner.cs
+++ MacroWave/Assets/Scripts/EnemySpawner.cs
@@ -1,21 +1,70 @@
 ﻿//This is the fault of Andrew
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+//using System;
+
 namespace Application
 {
 	public class EnemySpawner : MonoBehaviour
 	{
 		private GameObject Spawner;
+        private int enemyCount;
+        public bool spawnerActive;
+        private List<GameObject> activeEnemies = new List<GameObject>(); //List that holds all enemy objects
+        private List<GameObject> nextWave = new List<GameObject>(); //List of next enemies to spawn
 
-		//Attaches to the Spawner GameObject
-		void Start(){
-			Spawner = GameObject.Find("Spawner");
+        //Attaches to the Spawner GameObject
+        void Start(){     
+            Spawner = GameObject.Find("Spawner");
+            enemyCount = activeEnemies.Count;
+            spawnerActive = false;
 		}
 
-		//Spawns enemy at the Spawn Gate's position
-		public void SpawnEnemy(GameObject Enemy){
-			Instantiate (Enemy, Spawner.transform.position);
+        void update()
+        {
+            enemyCount = activeEnemies.Count;
+            if (!spawnerActive && enemyCount <= 0) spawnMore(nextWave);
+            else if (enemyCount > 0) spawnerActive = true;
+        }
+
+        //Spawns all enemies slated to spawn for next wave from List of enemies to spawn
+        private IEnumerator spawnMore(List<GameObject> Enemies)
+        {
+            spawnerActive = false;
+            List<GameObject> enemiesToSpawn = Enemies;
+            foreach (GameObject enemy in enemiesToSpawn)
+            {
+                SpawnEnemy(enemy);
+                enemyCount += 1;
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        //Spawns enemy at the Spawn Gate's position
+        public void SpawnEnemy(GameObject Enemy){
+            activeEnemies.Add(Enemy);
+			Instantiate (Enemy, Spawner.transform.position, Quaternion.identity);
 		}
 
+        //Method for when enemy is defeated, might move to Wave controller
+        public void DespawnEnemy(GameObject Enemy)
+        {
+            activeEnemies.Remove(Enemy);
+            enemyCount -= 1;
+            Destroy(Enemy);
+        }
+
+        //Get List of enemies to spawn for next wave
+        public List<GameObject> getEnemiesToSpawn()
+        {
+            return nextWave;
+        }
+
+        //Set List of enemies to spawn for next wave
+        public void setEnemiesToSpawn(List<GameObject> array)
+        {
+            nextWave = array;
+        }
 	}
 }
