9d6b78f9c505d7f1a455101ac6d92ab43607d7e6
Whotsit <jacob.phong.le@gmail.com>
2017-01-21 16:51:13 -0800

    REPUSH

---
+++ MacroWave/Assets/Scripts/grapeCluster_controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grapeCluster_controller : MonoBehaviour {
+	private GameObject player;
+	public GameObject grape;
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float step = speed * Time.deltaTime;
+		var player_pos = player.transform.position;
+		float distanceToPlayer = Mathf.Sqrt(Mathf.Pow((transform.position.x -player_pos.x),2) + Mathf.Pow((transform.position.y -player_pos.y),2));
+		if (distanceToPlayer <= 5.0f) {
+			Instantiate(grape, transform.position, transform.rotation);
+			Instantiate(grape, transform.position, transform.rotation);
+			Instantiate(grape, transform.position, transform.rotation);
+			Instantiate(grape, transform.position, transform.rotation);
+			Destroy (this.gameObject);
+			print ("exploding");
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, player_pos, step);//move towards function
+	}
+
+
+
+}
---
+++ MacroWave/Assets/Scripts/grape_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grape_controller : MonoBehaviour {
+	private float grapeSpeed = 0.5f;
+	private Rigidbody2D rb;
+	private int numGrape;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2 (grapeSpeed * Random.Range (-10, 10), grapeSpeed * Random.Range (-10, 10));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
